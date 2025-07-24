bfbcad1cee25efd0edf9d80254664155ed84d5f7
phillipsmara <phillipsmarac@gmail.com>
2025-07-23 20:09:14 -0400

    Delete Populate Continent Column.sql

--- Populate Continent Column.sql
+++
@@ -1,7 +0,0 @@
---populate continent column
-UPDATE StockData 
-SET Continent = CASE
-     WHEN Country IN ('USA', 'Canada', 'Bermuda') THEN 'North America'
-	 WHEN Country IN ('Ireland', 'Netherlands', 'Switzerland', 'United Kingdom') THEN 'Europe'
-	 WHEN Country IN ('Singapore') THEN 'Asia'
-END;
