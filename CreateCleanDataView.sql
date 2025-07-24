6865c93f6368c8e7126943d47185b1501e8cbda1
phillipsmara <phillipsmarac@gmail.com>
2025-07-23 20:08:48 -0400

    Delete CreateCleanDataView.sql

--- CreateCleanDataView.sql
+++
@@ -1,6 +0,0 @@
--- Create a clean stock data view with no null entries and only essential columns
-CREATE VIEW CleanStockData AS
-SELECT Symbol, Longname AS 'Name', Industry, Marketcap, Ebitda AS 'Earnings Before Interest, Taxes, Depreciation, and Amortization', Revenuegrowth AS 'Revenue Growth', Country
-FROM StockData 
-WHERE Revenuegrowth > 0
-     AND Ebitda IS NOT NULL;
